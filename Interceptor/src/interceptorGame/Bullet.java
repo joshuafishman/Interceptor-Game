67c8a2df47b62a8f2123427a82dd99228cd36bec
joshua fishman <joshuafishman1@gmail.com>
2013-10-29 21:14:57 -0400

    Update Bullet.java

--- Interceptor/src/interceptorGame/Bullet.java
+++ Interceptor/src/interceptorGame/Bullet.java
@@ -1,78 +1,47 @@
 package interceptorGame;
 import java.awt.*;
-public class Bullet {
-  static int size;
-  int xcoord;
-  int ycoord;
-  static Color color;
-  double velX;
-  double velY;
-  double acc;
-  int dirX=1;
-  int dirY=1;
-  double speed=10;
-  int[] xvals= new int[3];
-  int[] yvals= new int[3];
-  double angle;
 
-  public Bullet(int x, int y,double a){
-    size=20;
-    xcoord=x;
-    ycoord=y;
-    angle=a;
-    velX=Math.cos(a)*speed;
-    velY=-Math.sin(a)*speed;
-    color=Color.DARK_GRAY;
-  }
-  public double distanceFrom(int x,int y){
-    double d=Math.sqrt(Math.pow(x-xcoord,2)+Math.pow(y-ycoord,2));
-    return d;
-  } 
-  public int getX(){
-    return xcoord;
-  }
-  public int getY(){
-    return ycoord;
-  }
-  public void moveX(double x){
-    xcoord+=x;
-  } 
-  public void moveY(double y){
-    ycoord+=y;
-  } 
-  public void explode(Graphics g){
-	  for(int i=0;i<700;i++){
-	  g.setColor(Color.ORANGE);
-	  g.drawOval(xcoord+dirX*size/2,ycoord+dirY*size/2,size,size);
-	  }
-  }
-  public void move(){
-	  if(velX<0){
-			dirX=1;
+public class Powerup {
+	String type;
+	double xcoord;
+	int ycoord;
+	double velX;
+	static int size;
+	
+	public Powerup(int x,double velX,String t){
+		xcoord=x;
+		ycoord=0;
+		this.velX=velX;
+		type=t;
+		size=30;
+	}
+	public double distanceFrom(int x,int y){
+	    double d=Math.sqrt(Math.pow(x-xcoord,2)+Math.pow(y-ycoord,2));
+	    return d;
+	} 	
+	public int getX(){
+	    return (int)(xcoord);
+	}
+	public int getY(){
+	    return ycoord;
+	}
+	public void move(){
+		ycoord+=4;
+		xcoord+=velX;
+		if(xcoord<size||xcoord>Game.size-size){
+			velX=-velX;
 		}
-		else {
-			dirX=-1;
+	}
+	public void paint (Graphics g){
+		if(type=="H"){
+		g.setColor(Color.RED);
 		}
-		if(velY<0){
-			dirY=1;
+		if(type=="B"){
+			g.setColor(Color.BLUE);
 		}
-		else {
-			dirY=-1;
-		}	  
-	    moveX((int)(velX));
-	    moveY((int)(velY));
-	    xvals[0]=(int)(xcoord+size*Math.cos(angle));
-	    xvals[1]=(int)(xcoord+size/3*Math.cos(angle-Math.PI/2));
-	    xvals[2]=(int)(xcoord+size/3*Math.cos(angle+Math.PI/2));
-	    yvals[0]=(int)(ycoord-size*Math.sin(angle));
-	    yvals[1]=(int)(ycoord-size/3*Math.sin(angle-Math.PI/2));
-        yvals[2]=(int)(ycoord-size/3*Math.sin(angle+Math.PI/2));
-  }
-  public void paint(Graphics g){
-    Color cPrev=color;
-    g.setColor(color);
-    g.fillPolygon(xvals,yvals,3);
-    g.setColor(cPrev);
-  }
-  
+	    g.fillOval((int)(xcoord-size/2),ycoord-size/2,size,size);
+	    g.setColor(Color.WHITE);
+	    g.setFont(new Font("Arial", Font.BOLD,20));
+	    g.drawString(new String(type),(int)(xcoord-7), ycoord+10);	
+	}
 }
